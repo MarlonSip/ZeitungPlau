cfc8d9c38187004d6e82b1a5079f052fc7af0dbe
MarlonSip <MarlonSip@github.com>
2023-12-20 19:57:21 +0100

    Hello World

---
+++ menu.js
@@ -0,0 +1,2 @@
+// menu.js
+document.getElementById('app').innerHTML = '<iframe src="menu.html" frameborder="0" width="100%" height="60"></iframe>';
---
+++ test.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+test_dict = {'Marlon': ['29-04-1996', '176'],
+    'Ash': ['10-04-1996', '158']}
